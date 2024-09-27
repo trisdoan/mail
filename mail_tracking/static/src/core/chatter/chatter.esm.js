2f6c0c0ce7dc1e07fb4c4bd142523da429345ed6
trisdoan <doanminhtri8183@gmail.com>
2024-09-27 18:18:08 +0700

    [MIG] mail_tracking: Migration to 18.0

--- mail_tracking/static/src/core/chatter/chatter.esm.js
+++ mail_tracking/static/src/core/chatter/chatter.esm.js
@@ -1,19 +1,20 @@
-/** @odoo-module */
-import {Chatter} from "@mail/core/web/chatter";
+import { Chatter } from "@mail/chatter/web_portal/chatter";
 import {FailedMessage} from "@mail_tracking/components/failed_message/failed_message.esm";
 import {FailedMessagesPanel} from "@mail_tracking/components/failed_messages_panel/failed_messages_panel.esm";
 import {patch} from "@web/core/utils/patch";
 
 const {useState} = owl;
-
-Chatter.components = {
-    ...Chatter.components,
+Object.assign(Chatter.components, {
     FailedMessage,
     FailedMessagesPanel,
-};
+});
 
-/** @type {import("@mail/core/common/chatter").Chatter} */
-const ChatterPatch = {
+/**
+ * @type {import("@mail/chatter/web_portal/chatter").Chatter }
+ * @typedef {Object} Props
+ * @property {function} [close]
+ */
+patch(Chatter.prototype, {
     setup() {
         super.setup(...arguments);
         this.state = useState({
@@ -36,6 +37,11 @@ const ChatterPatch = {
     closeSearchFailed() {
         this.state.isSearchFailedOpen = false;
     },
-};
-
-patch(Chatter.prototype, ChatterPatch);
+    async reloadParentView() {
+        console.log("testtt")
+        await this.props.saveRecord?.();
+        if (this.props.webRecord) {
+            await this.props.webRecord.load();
+        }
+    },
+});
